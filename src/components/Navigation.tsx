e79f93dc2f3da612ae78f7fb12375c49a9bdb18a
bskwizz <150848152+bskwizz@users.noreply.github.com>
2025-11-11 15:01:18 -0600

    Updated Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -38,7 +38,7 @@ export default function Navigation({ currentPage, onNavigate }: NavigationProps)
             <img
               src={`${import.meta.env.BASE_URL}New Wave Associates Horizontal (1).png`}
               alt="New Wave Associates"
-              className="h-22 md:h-28 w-auto"
+              className="h-28 md:h-36 w-auto"
             />
           </button>
 
