155dc1c3f12748adebad8a62f964f8d6ec0e2043
bskwizz <150848152+bskwizz@users.noreply.github.com>
2026-02-02 16:54:50 -0600

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',   // 👈 ADD THIS LINE
+  base: '/',
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+  },
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
