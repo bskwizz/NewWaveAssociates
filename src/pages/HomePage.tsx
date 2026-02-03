b1e781bdb8092062d58ede1b30b3934914442554
bskwizz <150848152+bskwizz@users.noreply.github.com>
2026-02-03 11:38:21 -0600

    Updated index.html

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -56,7 +56,7 @@ export default function HomePage({ onNavigate }: HomePageProps) {
               </div>
             </div>
             <div className="w-full bg-[#00a4dd] mt-12" style={{height: '75px'}}></div>
-            <div className="w-full flex items-center justify-center" style={{paddingTop: '40px', paddingBottom: '40px'}}>
+            <div className="w-full flex items-center justify-center" style={{paddingTop: '80px', paddingBottom: '80px'}}>
               <LogoScroller />
             </div>
           </div>
