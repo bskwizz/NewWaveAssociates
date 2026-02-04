4cd315299560e0ee8bc2e76041061873c8e7faa5
bskwizz <150848152+bskwizz@users.noreply.github.com>
2026-02-03 21:29:42 -0600

    Updated index.html

--- src/pages/OurServicesPage.tsx
+++ src/pages/OurServicesPage.tsx
@@ -300,7 +300,7 @@ export default function OurServicesPage({ onNavigate }: OurServicesPageProps) {
           <img
             src={`${import.meta.env.BASE_URL}wave_graphic_for_our_services_page.png`}
             alt="Wave graphic"
-            className="w-full h-full object-cover lg:object-contain object-bottom"
+            className="w-full h-full object-cover object-bottom lg:w-screen lg:max-w-none lg:relative lg:left-1/2 lg:-translate-x-1/2"
           />
         </div>
 
